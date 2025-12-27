29d8c0a02ba91d9ec0e2086b4a12c33c84a7f46b
Alex Jonsson <153451620+CKCHDX@users.noreply.github.com>
2025-12-27 20:57:10 +0100

    Add webpack config and entry point

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 8080,
+    hot: true,
+    open: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+  },
+  mode: 'development',
+  devtool: 'inline-source-map'
+};
